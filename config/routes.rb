4c51b8807e123346a3abc9c87ab2387742f2680a
Travis Shields <travis.shields@gmail.com>
2016-06-27 21:17:09 -0400

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
-  get 'search_stocks', to:"stocks#search"
+  get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
